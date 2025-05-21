46fc49561ac19438ee2e2ae1699881703f7a9f9b
tomoya yoshida <ihatov08@gmail.com>
2025-05-22 06:40:39 +0900

    Providerでラップする

--- app/(chat)/layout.tsx
+++ app/(chat)/layout.tsx
@@ -1,18 +1,21 @@
-import { Sidebar } from "@/components/sidebar";
+import { Sidebar, SidebarProvider } from "@/components/sidebar";
+import { cookies } from "next/headers";
 
 export default async function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const cookieStore = await cookies();
+  const expended = cookieStore.get("sidebar:state")?.value === "true";
   return (
     <>
-      <div className="group/sidebar-wrapper flex min-h-svh w-full">
+      <SidebarProvider defaultOpen={expended}>
         <Sidebar />
         <div className="relative flex min-h-svh flex-1 flex-col">
           {children}
         </div>
-      </div>
+      </SidebarProvider>
     </>
   );
 }
