bcbefa136032564d854d050e355ae9eae32c933a
tomoya yoshida <ihatov08@gmail.com>
2025-05-22 12:04:54 +0900

    prisma x vercel x neonで使えるように設定

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,16 @@
 import { PrismaClient } from "@/app/generated/prisma";
+import { PrismaNeon } from "@prisma/adapter-neon";
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const adapter = new PrismaNeon({ connectionString });
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
 export const prisma = globalForPrisma.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV === "production") {
+  globalForPrisma.prisma = new PrismaClient({ adapter });
+} else {
+  globalForPrisma.prisma = prisma;
+}
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
+// @ts-ignore: no declaration file for this module
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+    return config;
+  }
 };
 
 export default nextConfig;
